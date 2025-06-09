febeefa2101e3242b9ef236d9c1c8661161d8622
Erik Kowald <ek01992@proton.me>
2025-06-09 18:59:03 -0500

    Remove OpenGL package assignment from hardware-configuration.nix: streamline graphics configuration by eliminating the explicit setting of hardware.graphics.package.

--- hosts/xps/hardware-configuration.nix
+++ hosts/xps/hardware-configuration.nix
@@ -54,5 +54,4 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  hardware.graphics.package = "opengl";
 }
