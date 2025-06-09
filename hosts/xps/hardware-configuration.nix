1320d1c393652d9924eddaa808bbf216f64f6774
Erik Kowald <ek01992@proton.me>
2025-06-09 17:57:32 -0500

    Refactor hardware configuration: streamline the hardware section in hardware-configuration.nix by removing unnecessary nesting for the Intel microcode update, enhancing clarity and structure.

--- hosts/xps/hardware-configuration.nix
+++ hosts/xps/hardware-configuration.nix
@@ -51,8 +51,5 @@
   # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware = {
-    opengl.enable = true;
-    cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  };
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
