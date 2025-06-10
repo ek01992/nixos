b9983720c1a31b928c2a53b5901042b4e506e688
Erik Kowald <ek01992@proton.me>
2025-06-10 00:13:56 -0500

    Refactor Hyprland NixOS configuration: remove font packages and redundant window manager settings for a cleaner setup and improved readability.

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -11,35 +11,12 @@
     enable = true;
     xwayland.enable = true;
   };
-
+  
   xdg.portal = {
     enable = true;
     extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
   };
 
-  fonts.packages = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    font-awesome
-  ];
-
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
-  
-  wayland.windowManager.hyprland = {
-    enable = true;
-    package = null;
-    portalPackage = null;
-    settings = {
-    "$mod" = "SUPER";
-    bind =
-      [
-        "$mod, F, exec, firefox"
-        ", Print, exec, grimblast copy area"
-        "$mod, T, exec, kitty"
-        "$mod, D, exec, rofi -show drun"
-      ];
-    };
-  };
 }
