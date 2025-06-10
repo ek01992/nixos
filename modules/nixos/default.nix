274db41e1a66b050566bddc1a82824ad54221909
Erik Kowald <ek01992@proton.me>
2025-06-09 21:21:51 -0500

    Refactor NixOS configuration: move Nix settings for flakes and Cachix from default.nix to hyprland.nix for improved modularity and organization.

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -10,14 +10,6 @@
   i18n.defaultLocale = "en_US.UTF-8";
   nixpkgs.config.allowUnfree = true;
 
-  # Nix settings for flakes and Cachix
-  nix.settings = {
-    experimental-features = [ "nix-command" "flakes" ];
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-  };
-
   # Core packages needed on the system
   environment.systemPackages = with pkgs; [
     git
--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -1,5 +1,13 @@
 { pkgs, ... }:
 {
+
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
+  
   # System-wide Hyprland configuration
   programs.hyprland = {
     enable = true;
