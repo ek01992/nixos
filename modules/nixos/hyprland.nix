255c6ce9eddf09f958a28e8ac5482327ef47bc76
Erik Kowald <ek01992@proton.me>
2025-06-09 23:24:02 -0500

    Add Hyprland configuration to NixOS: enable programs, xdg portal, and include essential font packages for enhanced user experience.

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -1,5 +1,21 @@
 { pkgs, ... }:
 {
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+  };
+
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    font-awesome
+  ];
   wayland.windowManager.hyprland = {
     enable = true;
     package = null;
