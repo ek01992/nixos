e8fb9d2e10f4dc11b4da318864aa5f16fe6bf3ec
Erik Kowald <ek01992@proton.me>
2025-06-09 21:22:26 -0500

    Update NixOS configuration: reintroduce experimental features for Nix settings in default.nix to enable 'nix-command' and 'flakes' support.

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -5,6 +5,8 @@
     ./fonts.nix
   ];
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  
   # Basic system settings
   time.timeZone = "America/Chicago";
   i18n.defaultLocale = "en_US.UTF-8";
