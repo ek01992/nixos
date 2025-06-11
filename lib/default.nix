715c0680a0d5582a6f27c919b3b8c4e926607c78
Erik Kowald <ek01992@proton.me>
2025-06-10 20:34:41 -0500

    Remove base.nix file and integrate its configuration directly into default.nix for improved clarity and maintainability of NixOS setup.

--- lib/default.nix
+++ lib/default.nix
@@ -1,5 +1,13 @@
+{ inputs, ... }:
 {
-  imports = [
-    ./base.nix
-  ];
+  nixosSystem = { system, hostName, specialArgs, modules }:
+    inputs.nixpkgs.lib.nixosSystem {
+      inherit system specialArgs;
+      modules = [
+        ({ config, pkgs, ... }: {
+          networking.hostName = hostName;
+          system.stateVersion = "25.05";
+        })
+      ] ++ modules;
+    };
 }
