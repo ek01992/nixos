417f9fa9c40e3d9eda0e1e2a20c4a0480d277a64
Erik Kowald <ek01992@proton.me>
2025-06-10 22:54:43 -0500

    Add hardware configuration for graphics support in NixOS: enable graphics hardware, include necessary packages for Intel media processing, and enhance overall system capabilities.

--- modules/desktops/wm/wayland/hyprland.nix
+++ modules/desktops/wm/wayland/hyprland.nix
@@ -3,7 +3,14 @@
   options.modules.desktops.wm.wayland.hyprland.enable = lib.mkEnableOption "hyprland wayland windows manager";
 
   config = lib.mkIf config.modules.desktops.wm.wayland.hyprland.enable {
-    programs.hyprland.enable = true;
+    programs.hyprland = {
+      enable = true;
+      withUWSM = true;
+      package = pkgs.hyprland;
+      portalPackage = pkgs.xdg-desktop-portal-hyprland;
+    };
+
+    programs.uwsm.enable = true;
 
     environment.systemPackages = with pkgs; [
       rofi-wayland
