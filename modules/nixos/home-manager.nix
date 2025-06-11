77595b5a4fdc65fcb7a33ca2c480c4af96946a35
Erik Kowald <ek01992@proton.me>
2025-06-10 21:42:35 -0500

    Refactor NixOS home-manager configuration: streamline imports by consolidating home-manager module inclusion, enhancing clarity and maintainability of the setup.

--- modules/nixos/home-manager.nix
+++ modules/nixos/home-manager.nix
@@ -1,12 +1,12 @@
 { lib, config, inputs, ... }:
 {
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+  ];
+
   options.modules.nixos.home-manager.enable = lib.mkEnableOption "home-manager";
 
   config = lib.mkIf config.modules.nixos.home-manager.enable {
-    imports = [
-      inputs.home-manager.nixosModules.home-manager
-    ];
-
     home-manager = {
       useGlobalPkgs = true;
       useUserPackages = true;
