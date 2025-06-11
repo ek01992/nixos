e018a6bb80edb6026811c5ab5ed59503be5f1efb
Erik Kowald <ek01992@proton.me>
2025-06-10 22:45:27 -0500

    Update Hyprland NixOS module: modify enable option description to clarify its purpose as a Wayland window manager.

--- modules/desktops/wm/wayland/hyprland.nix
+++ modules/desktops/wm/wayland/hyprland.nix
@@ -1,6 +1,6 @@
 { lib, config, pkgs, ... }:
 {
-  options.modules.desktops.wm.wayland.hyprland.enable = lib.mkEnableOption "hyprland";
+  options.modules.desktops.wm.wayland.hyprland.enable = lib.mkEnableOption "hyprland wayland windows manager";
 
   config = lib.mkIf config.modules.desktops.wm.wayland.hyprland.enable {
     programs.hyprland.enable = true;
