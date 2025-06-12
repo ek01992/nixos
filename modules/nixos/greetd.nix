bbd3784cc54d72628e1711fb858bcbed7e6675be
Erik Kowald <ek01992@proton.me>
2025-06-11 19:57:14 -0500

    Remove 'vt' setting from greetd configuration in NixOS: streamline greetd.nix for improved clarity and functionality.

--- modules/nixos/greetd.nix
+++ modules/nixos/greetd.nix
@@ -5,7 +5,6 @@
   config = lib.mkIf config.modules.nixos.greetd.enable {
     services.greetd = {
       enable = true;
-      vt = 1;
       settings = {
         default_session = {
           command = "${pkgs.greetd.greetd}/bin/agreety --cmd hyprland";
