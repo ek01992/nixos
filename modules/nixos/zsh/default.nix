73c8b85b0e96a72fc079f7f2e9ed44340058891a
Erik Kowald <ek01992@proton.me>
2025-06-13 18:27:47 -0500

    Remove zplug plugin definitions from Zsh configuration in NixOS
    - Eliminate specific plugin entries for zsh-autosuggestions and powerlevel10k from the zplug configuration, streamlining the setup.
    - Maintain zplug functionality while simplifying the overall Zsh configuration.

--- modules/nixos/zsh/default.nix
+++ modules/nixos/zsh/default.nix
@@ -9,10 +9,6 @@ in
     programs.zsh = {
       zplug = {
         enable = true;
-        plugins = [
-          { name = "zsh-users/zsh-autosuggestions"; }
-          { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; }
-        ];
       };
       enable = true;
       enableCompletion = true;
