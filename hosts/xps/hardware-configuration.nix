968c2ddaf55c8b9c5aee35c035c5a0174803fc29
Erik Kowald <ek01992@proton.me>
2025-06-12 21:52:37 -0500

    Remove plymouth theme configuration from hardware-configuration.nix for cleaner setup.

--- hosts/xps/hardware-configuration.nix
+++ hosts/xps/hardware-configuration.nix
@@ -33,13 +33,6 @@
     extraModulePackages = [ ];
     plymouth = {
       enable = true;
-      theme = "rings";
-      themePackages = with pkgs; [
-        # By default we would install all themes
-        (adi1090x-plymouth-themes.override {
-          selected_themes = [ "rings" ];
-        })
-      ];
     };
   }; 
   
