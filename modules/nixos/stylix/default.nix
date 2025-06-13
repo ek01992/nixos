d7a8ca7b9f82eb8ed1fca026ee718b85bb1c0fa7
Erik Kowald <ek01992@proton.me>
2025-06-12 21:48:22 -0500

    Fix sha256 hash format for image URL in stylix configuration for NixOS to ensure proper integrity verification.

--- modules/nixos/stylix/default.nix
+++ modules/nixos/stylix/default.nix
@@ -18,7 +18,7 @@ in
       enable = true;
       image = pkgs.fetchurl {
         url = "https://raw.githubusercontent.com/ek01992/wal/refs/heads/main/qhj152f5dc3f1.jpeg";
-        sha256 = "sha256-32d1e9307e1745bf55227135b9c6a16cff63115571edbd58eb3bd2df7a6700be";
+        sha256 = "32d1e9307e1745bf55227135b9c6a16cff63115571edbd58eb3bd2df7a6700be";
       };
     };
   };
