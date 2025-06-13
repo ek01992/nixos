c49b70452086b92ff9290a1943ab5df4e948e97b
Erik Kowald <ek01992@proton.me>
2025-06-13 00:35:48 -0500

    Remove deprecated Firefox extensions from home-manager configuration in default.nix to streamline setup and improve clarity.

--- modules/home-manager/gui/firefox/default.nix
+++ modules/home-manager/gui/firefox/default.nix
@@ -6,11 +6,6 @@ in
   options.my.gui.firefox.enable = lib.mkEnableOption "firefox";
 
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = with pkgs.nur.repos.rycee.firefox-addons; [
-      ublock-origin
-      proton-pass
-      dracula-dark-colorscheme
-    ];
     programs.firefox = {
       enable = true;
       extensions = with pkgs.nur.repos.rycee.firefox-addons; [
--- modules/nixos/home-manager/default.nix
+++ modules/nixos/home-manager/default.nix
@@ -17,7 +17,7 @@ in
       extraSpecialArgs = { inherit inputs; };
     };
     nixpkgs.overlays = [
-      inputs.nur.overlay
+      inputs.nur.overlay.default
     ];
   };
 }
