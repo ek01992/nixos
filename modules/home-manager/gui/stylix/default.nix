bea5472d2cae0d2dffacd04931d3674587a256c2
Erik Kowald <ek01992@proton.me>
2025-06-12 22:59:20 -0500

    Add base16 color scheme and font configurations to stylix in home-manager for enhanced theming.

--- modules/home-manager/gui/stylix/default.nix
+++ modules/home-manager/gui/stylix/default.nix
@@ -13,11 +13,26 @@ in
 
     stylix = {
       enable = true;
+      base16Scheme = "dracula";
       image = pkgs.fetchurl {
         url = "https://raw.githubusercontent.com/ek01992/wal/refs/heads/main/qhj152f5dc3f1.jpeg";
         sha256 = "32d1e9307e1745bf55227135b9c6a16cff63115571edbd58eb3bd2df7a6700be";
       };
+      fonts = {
+        serif = config.stylix.fonts.monospace;
+        sansSerif = config.stylix.fonts.monospace;
+        monospace = {
+          package = pkgs.nerdfonts;
+          name = "JetBrainsMono Nerd Font Mono";
+        };
+        emoji = {
+          package = pkgs.noto-fonts-emoji;
+          name = "Noto Color Emoji";
+        };
+      };
       targets.kitty.enable = true;
+      targets.hyprland.enable = true;
+
     };
   };
 }
