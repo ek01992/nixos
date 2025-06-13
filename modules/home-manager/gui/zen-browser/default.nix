9956bebaad3f82d2c2d77501281ecf635fc52c35
Erik Kowald <ek01992@proton.me>
2025-06-13 00:01:50 -0500

    Add extension settings for uBlock Origin, Privacy Badger, and Proton Pass in zen-browser configuration for enhanced privacy and security.

--- modules/home-manager/gui/zen-browser/default.nix
+++ modules/home-manager/gui/zen-browser/default.nix
@@ -13,8 +13,21 @@ in
     programs.zen-browser = {
       enable = true;
       policies = {
-        DisableAppUpdate = true;
         DisableTelemetry = true;
+        ExtensionSettings = {
+          "uBlock0@raymondhill.net" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+            installation_mode = "force_installed";
+          };
+          "jid1-MnnxcxisBPnSXQ@jetpack" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
+            installation_mode = "force_installed";
+          };
+          "78272b6fa58f4a1abaac99321d503a20@proton.me" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/proton-pass/latest.xpi";
+            installation_mode = "force_installed";
+          };
+        };
       };
     };
   };
