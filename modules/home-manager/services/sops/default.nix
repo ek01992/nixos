1ca360e3df6014167d0e822ed0e844b04625f885
Erik Kowald <ek01992@proton.me>
2025-06-13 11:15:55 -0500

    Refactor SOPS configuration in default.nix to align with updated service structure, changing path from `my.sops` to `my.services.sops` for improved clarity and organization.

--- modules/home-manager/services/sops/default.nix
+++ modules/home-manager/services/sops/default.nix
@@ -1,23 +1,13 @@
-/*
 { lib, config, inputs, ... }:
 let
-  cfg = config.my.sops;
+  cfg = config.my.services.sops;
 in
 {
-  options.my.sops.enable = lib.mkEnableOption "sops";
+  options.my.services.sops.enable = lib.mkEnableOption "sops";
 
   imports = [
-    inputs.sops-nix.homeManagerModules.sops
   ];
 
   config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [
-      age
-    ];
-    sops = {
-      defaultSopsFile = ./secrets.yaml;
-    };
   };
 }
-*/
-{}
