6aac2fde371a1633a17b36caa43d4712a9d10ce6
Erik Kowald <ek01992@proton.me>
2025-06-12 21:04:08 -0500

    Fix hardware configuration: ensure proper placement of timeout setting under systemd-boot for improved clarity and maintainability.

--- hosts/xps/hardware-configuration.nix
+++ hosts/xps/hardware-configuration.nix
@@ -24,8 +24,8 @@
       systemd-boot = {
         enable = true;
         configurationLimit = 5;
-        timeout = 0;
       };
+      timeout = 0;
       efi.canTouchEfiVariables = true;
     };
     kernelModules = [ "kvm-intel" ];
