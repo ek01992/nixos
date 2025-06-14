e308adb263051a10991d08d75bb0f890ca2f7629
Erik Kowald <ek01992@proton.me>
2025-06-13 22:17:10 -0500

    Add sops support to NixOS configuration
    - Enable sops in the home-manager configuration for enhanced secret management.
    - Update default.nix to include sops options and configuration settings.
    - Introduce a new sops.yaml file for defining creation rules and key groups.

--- modules/nixos/sops/default.nix
+++ modules/nixos/sops/default.nix
@@ -1,1 +1,12 @@
-{}
+{ config, lib, pkgs, ... }:
+{
+  options.nixos.sops.enable = lib.mkEnableOption "sops";
+
+  config = lib.mkIf config.nixos.sops.enable {
+    sops = {
+      defaultSopsFile = ../../../secrets/sops.yaml;
+      age.keyFile = "/var/lib/sops/age/keys.txt";
+      age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+    };
+  };
+}
