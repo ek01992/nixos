529911709a5736d0af5387617adedaeb1aecb378
Erik Kowald <ek01992@proton.me>
2025-06-13 21:34:41 -0500

    Update wofi configuration in default.nix
    - Correctly reference wofi in the GUI configuration by updating the variable from waybar to wofi.
    - Ensure the enablement option for wofi is properly set within the home-manager structure.

--- modules/home-manager/gui/wofi/default.nix
+++ modules/home-manager/gui/wofi/default.nix
@@ -1,9 +1,9 @@
 { lib, config, pkgs, ... }:
 let
-  cfg = config.gui.waybar;
+  cfg = config.gui.wofi;
 in
 {
-  options.gui.waybar.enable = lib.mkEnableOption "waybar";
+  options.gui.wofi.enable = lib.mkEnableOption "wofi";
 
   config = lib.mkIf cfg.enable {
     programs.wofi = {
