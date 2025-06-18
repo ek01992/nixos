541b7d17b5f57a6e302900c6a27dac0ad38c9786
Erik Kowald <ek01992@proton.me>
2025-06-18 17:36:13 -0500

    Add image and cursor settings to stylix configuration in default.nix
    - Introduce a new image fetched from a URL to enhance the visual theme.
    - Add cursor settings using the Bibata-Modern-Classic cursor package for improved user experience.
    - Ensure hyprland settings are streamlined for better configuration clarity.

--- modules/home-manager/gui/stylix/default.nix
+++ modules/home-manager/gui/stylix/default.nix
@@ -23,20 +23,28 @@ in
           name = "Noto Color Emoji";
         };
       };
+
+      image = pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/ek01992/wal/refs/heads/main/qhj152f5dc3f1.jpeg";
+        sha256 = "32d1e9307e1745bf55227135b9c6a16cff63115571edbd58eb3bd2df7a6700be";
+      };
+
+      cursor = {
+        package = pkgs.bibata-cursors;
+        name = "Bibata-Modern-Classic";
+        size = 24;
+      };
+
       targets = {
         kitty.enable = true;
         gtk.enable = true;
-        hyprland = {
-          enable = true;
-          hyprpaper.enable = true;
-        };
+        hyprland.enable = true;
         firefox = {
           enable = true;
           profileNames = [
             "erik"
           ];
         };
-        waybar.enable = true;
       };
     };
   };
