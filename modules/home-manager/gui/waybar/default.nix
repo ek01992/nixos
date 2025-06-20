1042dfb7ff17c585baee1d5be597f430b4d6c300
Erik Kowald <ek01992@proton.me>
2025-06-19 20:00:47 -0500

    Update icon definitions in Waybar Nix configuration to include font size specification of 24px for improved visibility and consistency.

--- modules/home-manager/gui/waybar/default.nix
+++ modules/home-manager/gui/waybar/default.nix
@@ -157,10 +157,10 @@ with lib; {
             tooltip = true;
             tooltip-format = "Current screen brightness: {percent}%";
             format-icons = [
-              "<span color='#696969'> 󰃞 </span>"
-              "<span color='#A9A9A9'> 󰃝 </span>"
-              "<span color='#FFFF66'> 󰃟 </span>"
-              "<span color='#FFD700'> 󰃠 </span>"
+              "<span size = '24px' color='#696969'> 󰃞 </span>"
+              "<span size = '24px' color='#A9A9A9'> 󰃝 </span>"
+              "<span size = '24px' color='#FFFF66'> 󰃟 </span>"
+              "<span size = '24px' color='#FFD700'> 󰃠 </span>"
             ];
           };
           "bluetooth" = {
