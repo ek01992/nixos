077627c56f70ace94980b001e977ec38c4ace454
Erik Kowald <ek01992@proton.me>
2025-06-22 16:18:31 -0500

    Rename Nixfmt program configuration to 'nixfmt-rfc-style' for clarity and consistency in NixOS setup.

--- modules/nixos/nixfmt/default.nix
+++ modules/nixos/nixfmt/default.nix
@@ -10,7 +10,7 @@ in
 
   config = lib.mkIf cfg.enable {
     environment.systemPackages = [ pkgs.nixfmt-rfc-style ];
-    programs.nixfmt = {
+    programs.nixfmt-rfc-style = {
       enable = true;
     };
   };
