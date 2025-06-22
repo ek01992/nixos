d31c3219cbc999eb8b93f38ddc3e96aa09f07075
Erik Kowald <ek01992@proton.me>
2025-06-22 17:49:58 -0500

    Update Starship Nix configuration to increase command timeout to 1000 and adjust formatting for improved readability of the shell prompt components.

--- modules/home-manager/cli/starship/default.nix
+++ modules/home-manager/cli/starship/default.nix
@@ -13,15 +13,15 @@ in
       enable = true;
       settings = {
         add_newline = true;
-        command_timeout = 500;
+        command_timeout = 1000;
         continuation_prompt = "[∙](bright-black) ";
         format = ''
           [](0x9A348E)$username$hostname$localip$singularity$kubernetes
-          [](fg:0x9A348E bg:0xDA627D)$directory$vcsh
-          [](fg:0xDA627D bg:0xFCA17D)$git_branch$git_commit$git_state$git_metrics$git_status
-          [](fg:0xFCA17D bg:0x86BBD8)$c$cmake$cobol$container$daml$dart$deno$dotnet$elixir$elm$erlang$golang$haskell$helm$java$julia$kotlin$lua$nim$nodejs$ocaml$perl$php$pulumi$purescript$python$rlang$red$ruby$rust$scala$swift$terraform$vlang$vagrant$zig
-          [](fg:0x86BBD8 bg:0x06969A)$docker_context$package$buf$nix_shell$conda$spack$memory_usage$aws$gcloud$openstack$env_var$crystal$custom$cmd_duration$jobs$battery
-          [](fg:0x06969A bg:0x33658A)$time$status$shell$line_break$character
+          ($directory$vcsh)(fg:0x9A348E bg:0xDA627D)
+          ([](fg:0xDA627D bg:0xFCA17D)$git_branch$git_commit$git_state$git_metrics$git_status)
+          ([](fg:0xFCA17D bg:0x86BBD8)$c$cmake$cobol$container$daml$dart$deno$dotnet$elixir$elm$erlang$golang$haskell$helm$java$julia$kotlin$lua$nim$nodejs$ocaml$perl$php$pulumi$purescript$python$rlang$red$ruby$rust$scala$swift$terraform$vlang$vagrant$zig)
+          ([](fg:0x86BBD8 bg:0x06969A)$docker_context$package$buf$nix_shell$conda$spack$memory_usage$aws$gcloud$openstack$env_var$crystal$custom$cmd_duration$jobs$battery)
+          ([](fg:0x06969A bg:0x33658A)$time$status$shell$line_break$character)
         '';
         right_format = "";
         scan_timeout = 30;
@@ -437,7 +437,7 @@ in
         hostname = {
           disabled = false;
           format = "[$ssh_symbol$hostname]($style) ";
-          ssh_only = true;
+          ssh_only = false;
           ssh_symbol = "󰢹 ";
           style = "green dimmed bold";
           trim_at = ".";
