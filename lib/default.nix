847c5ba3f126dbdb7032dff865d02f57ebb092d4
Erik Kowald <erik@erik-dev.io>
2025-10-27 00:48:12 -0500

    fix(lib): adjust import formatting in default.nix
    - Updated `lib/default.nix` to standardize the formatting of module imports.
    - This change improves readability and maintains consistency with the overall codebase structure, aligning with recent refactoring efforts to enhance clarity and maintainability.

--- lib/default.nix
+++ lib/default.nix
@@ -1,4 +1,4 @@
 {lib, ...}:
-  (import ./mkModule {inherit lib;})
-  // (import ./mkSecret {inherit lib;})
-  // (import ./mkSystemdService {inherit lib;})
+(import ./mkModule {inherit lib;})
+// (import ./mkSecret {inherit lib;})
+// (import ./mkSystemdService {inherit lib;})
