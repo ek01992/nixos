a4f3492ca5f9f38609fa4c30d8643c4e9fc62493
Erik <ek01992@proton.me>
2026-04-18 22:27:09 -0500

    fix

--- modules/hosts/wsl/default.nix
+++ modules/hosts/wsl/default.nix
@@ -3,7 +3,7 @@
   flake.nixosConfigurations.nixos-wsl = inputs.nixpkgs.lib.nixosSystem {
     modules =[
       self.nixosModules.nixos-wslConfiguration
-      inputs.nixos-wsl.nixosModules.default
+      nixos-wsl.nixosModules.default
     ];
   };
 }
