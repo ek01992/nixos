4f977be86790fda047f92798336ec7c1ef7a7fd0
Erik <ek01992@proton.me>
2026-04-21 22:52:53 -0500

    fix network

--- modules/hosts/nixxy/configuration.nix
+++ modules/hosts/nixxy/configuration.nix
@@ -51,12 +51,7 @@
 
     networking = {
       hostName = "nixxy";
-      networkmanager.enable = false;
-      wireless = {
-        enable = true;
-        userControlled = true;
-        networks.Wiffy.pskRaw = "b6a859f735233e8dd7ae33141c041526259be3b79b36eba6d3dfb04848dac58b";
-      };
+      networkmanager.enable = true;
     };
 
     i18n = {
